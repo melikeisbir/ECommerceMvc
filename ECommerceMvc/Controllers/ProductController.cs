6da8b009dd6449e42ab346812d7b0283a32dfc8a
Melike İsbir <melikeisbr@gmail.com>
2024-05-04 19:54:45 +0300

    list, edit, details, delete

--- ECommerceMvc/Controllers/ProductController.cs
+++ ECommerceMvc/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerceMvc.Entity;
 using ECommerceMvc.Models;
+using ECommerceMvc.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,16 +19,156 @@ namespace ECommerceMvc.Controllers
         public async Task<IActionResult> Index()
         {
             List<Product> urunListesi = new List<Product>();
-            urunListesi = await _context.Products.ToListAsync(); //select * from Kitap 
+            urunListesi = await _context.Products.ToListAsync();
             return View(urunListesi);
         }
-            public IActionResult Detail()
+        public IActionResult Create()
         {
             return View();
         }
-        public IActionResult List()
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewProductViewModel model)
         {
-            return View();
+            try
+            {
+                string yuklenenResimAdi = ResimYukle(model);
+                Product urun = new Product
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Price = model.Price,
+                    Stock = model.Stock,
+                    ProductImage = yuklenenResimAdi
+                };
+
+                _context.Add(urun);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        private string ResimYukle(NewProductViewModel model)
+        {
+            string dosyaAdi = "";
+            string dosyaninYuklenecegiKlasorYolu = Path.Combine(_environment.WebRootPath, "Uploads");
+
+            if (!Directory.Exists(dosyaninYuklenecegiKlasorYolu))
+            {
+                Directory.CreateDirectory(dosyaninYuklenecegiKlasorYolu);
+            }
+
+            if (model.ProductPicture.FileName != null)
+            {
+                dosyaAdi = model.ProductPicture.FileName;
+                string filePath = Path.Combine(dosyaninYuklenecegiKlasorYolu, dosyaAdi);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.ProductPicture.CopyTo(fileStream);
+                }
+
+            }
+            return dosyaAdi;
+        }
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var urunDetay = await _context.Products.FindAsync(id);
+
+            NewProductViewModel productViewModel = new()
+            {
+                Name = urunDetay.Name,
+                Description = urunDetay.Description,
+                Price = urunDetay.Price,
+                Stock = urunDetay.Stock,
+                ProductImage = urunDetay.ProductImage
+            };
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, NewProductViewModel model)
+        {
+            var urun = await _context.Products.FindAsync(model.Id);
+            urun.Name = model.Name;
+            urun.Description = model.Description;
+            urun.Price = model.Price;
+            urun.Stock = model.Stock;
+            //düzenleme sayfasında bir başka resim seçtiysem kontrolünü yapmam gerekiyro
+            if (model.ProductPicture != null)
+            {
+                //resmini değiştirmek istediğim ürünün database deki ProductPicture kolonundaki adına göre
+                // git wwwroot klasörü altındaki Uploads klasöründeki ilgili resmi bul ve sil
+                string filePath = Path.Combine(_environment.WebRootPath, "Uploads", urun.ProductImage);
+                System.IO.File.Delete(filePath);
+
+
+                Product guncellencekUrun = new Product();
+
+                guncellencekUrun.Id = model.Id;
+                guncellencekUrun.Description = model.Description;
+                guncellencekUrun.Price = model.Price;
+                guncellencekUrun.Stock = model.Stock;
+                string guncellenenYuklenenResimAdi = ResimYukle(model);
+                guncellencekUrun.Name = guncellenenYuklenenResimAdi;
+
+                _context.Entry(guncellencekUrun).State = EntityState.Modified;
+                try
+                {
+                    await _context.SaveChangesAsync(); //update 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //select * from Kitap where Id=id
+            var urun = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            string filePath = Path.Combine(_environment.WebRootPath, "Uploads", urun.ProductImage);
+            System.IO.File.Delete(filePath);
+            _context.Products.Remove(urun);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var urunDetay = await _context.Products.FindAsync(id);
+
+            NewProductViewModel productViewModel = new()
+            {
+                Name = urunDetay.Name,
+                Description = urunDetay.Description,
+                Price = urunDetay.Price,
+                Stock = urunDetay.Stock,
+                ProductImage = urunDetay.ProductImage
+            };
+            return View(productViewModel);
         }
     }
 }
--- ECommerceMvc/ViewModels/NewProductViewModel.cs
+++ ECommerceMvc/ViewModels/NewProductViewModel.cs
@@ -5,8 +5,6 @@ namespace ECommerceMvc.ViewModels
     public class NewProductViewModel : EditImageViewModel
     {
         [Required]
-        public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
         [Required]
         public string Description { get; set; }
